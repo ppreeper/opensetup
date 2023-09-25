65e97ff7a1ec55b02035a6fb11ad7ed1c4f18ce1
Peter Preeper <ppreeper@gmail.com>
2023-09-25 09:57:03 -0600

    update js lang

--- src/role_command.sh
+++ src/role_command.sh
@@ -1,3 +1,3 @@
-[ "${args[playbook]}" = "localhost" ] && export BECOME="-K" || export BECOME=""
+[ "${args[target]}" = "localhost" ] && export BECOME="-K" || export BECOME=""
 echo ansible-playbook -l ${args[target]} -e role=${args[role]} -b ${PLAYBOOK_DIR}/apply_role.yml $BECOME
 ansible-playbook -l ${args[target]} -e role=${args[role]} -b ${PLAYBOOK_DIR}/apply_role.yml $BECOME
